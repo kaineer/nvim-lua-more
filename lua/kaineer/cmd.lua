9124d954b76f671d477a652dc30145bb981427f1
Sergey Kluchkovsky <kaineer@gmail.com>
2023-05-07 22:53:08 +0500

    Store at 07.05.2023 22:53

--- lua/kaineer/cmd.lua
+++ lua/kaineer/cmd.lua
@@ -1,8 +1,11 @@
 return {
   buildMkcmd = function(message)
     return function(name) -- mkcmd()
+      local runcmd = function()
+        vim.api.nvim_command(":" .. name)
+      end
       return function()
-        if not pcall(function() vim.api.nvim_command(":" .. name) end) then
+        if not pcall(runcmd) then
           print(message)
         end
       end
